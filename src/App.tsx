87771eae1474ae5960fbe59c617902135204fdef
Ahmad Yasin <98221483+Ahmadyasin1@users.noreply.github.com>
2025-02-01 12:56:09 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -105,7 +105,7 @@ function App() {
         formData.append('image', blob, 'drawing.png');
       }
 
-      const response = await fetch('http://localhost:5000/predict', {
+      const response = await fetch('https://annlab.pythonanywhere.com/predict', {
         method: 'POST',
         body: formData,
       });
